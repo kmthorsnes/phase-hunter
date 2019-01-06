bdf3a490ca0c4ee27e8a769aea7de1212f7f0a9c
kalle <kmthorsnes@gmail.com>
2019-01-06 21:00:55 +0100

    wip

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+      }
+}
--- js/app.js
+++ js/app.js
@@ -3,4 +3,19 @@
  * app.js */
 
  console.log('💩Kalle er sjefen💩');
- 
+
+ const phrase = new Phrase();
+ const game = new Game();
+
+
+
+
+ // app.js to create a new instance of the `Game` class and add event listeners for the start button and onscreen keyboard buttons.
+
+
+
+// document.getElementById("btn__reset").addEventListener("click", startGame);
+
+// function startGame() {
+//     console.log("start game clicked");
+// }
