0617f9b028653205c3b2dc90ea15de2f5007ee35
kalle <kmthorsnes@gmail.com>
2019-01-14 14:46:45 +0100

    finnish step 4

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,19 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
     }
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phrases = ["All we know is that we dont know",
+        "we are all part of a masterplan",
+        "de you keep the receip for the friends that you buy",
+        "dont look back in anger",
+        "take me to the place where you go where nobody knows if its night or day"];
+        return phrases;
+    };
 }
--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
  * Project 4 - OOP Game App
  * app.js */
 
-console.log('💩Kalle er sjefen💩');
-const phrase = new Phrase('Lorem ipsum dolor amet raclette slow-carb poke chicharrones mustache jianbing portland la croix cardigan put a bird on it. Kombucha franzen kogi, yr cardigan echo park cloud bread ennui viral adaptogen pug pok pok meditation master cleanse. Chia hammock cray, quinoa YOLO shaman chartreuse. ')
-console.log(`Phrase - phrase: ${phrase.phrase}`);
+const game = new Game();
+
+game.phrases.forEach((phrase, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
