2a14c4b3b19303b114e01b0d7cf55a7b38cb5f58
kalle <kmthorsnes@gmail.com>
2019-01-15 15:41:04 +0100

    why is this not working

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,13 @@ class Phrase {
     constructor(phrase){
         this.phrase = phrase.toLowerCase();
     }
-}
+
+    addPhraseToDisplay(){
+        const selectedPhrase = Game.getRandomPhrase();
+        var str = "";
+        for (var i = 0; i < selectedPhrase.length; i++) {
+            str += '<li>'+ selectedPhrase.charAt(i) + '</li>';
+        }
+        document.getElementById("phrase").innerText = str; 
+    }
+};
--- js/app.js
+++ js/app.js
@@ -2,15 +2,8 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const logPhrase = phrase => {
-  console.log(`Phrase - phrase: `, phrase.phrase);
-};
 
 const game = new Game();
+game.getRandomPhrase()
 
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
 
