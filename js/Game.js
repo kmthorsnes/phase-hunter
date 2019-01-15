b7a3e1b02105c9b1a66d1038c9c7d35d41ff2dd6
kalle <kmthorsnes@gmail.com>
2019-01-15 09:25:31 +0100

    from array of strings, to object of arrays

--- js/Game.js
+++ js/Game.js
@@ -3,21 +3,26 @@
  * Game.js */
 
 class Game {
-    constructor() {
-        this.missed = 0;
-        this.phrases = this.createPhrases();
-        this.activePhrase = null;
-    }
-    /**
-    * Creates phrases for use in game
-    * @return {array} An array of phrases that could be used in the game
-    */
-    createPhrases() {
-        const phrases = ["All we know is that we dont know",
-        "We are all part of a masterplan",
-        "Do you keep the receipt for the friends that you buy",
-        "Dont look back in anger",
-        "Take me to the place where you go where nobody knows if its night or day"];
-        return phrases;
-    };
+  constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+  }
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases() {
+    const phrases = [
+      { phrase: "All we know is that we dont know" },
+      { phrase: "We are all part of a masterplan" },
+      { phrase: "Do you keep the receipt for the friends that you buy" },
+      { phrase: "Dont look back in anger" },
+      {
+        phrase:
+          "Take me to the place where you go where nobody knows if its night or day"
+      }
+    ];
+    return phrases;
+  }
 }
--- js/app.js
+++ js/app.js
@@ -5,5 +5,5 @@
 const game = new Game();
 
 game.phrases.forEach((phrase, index) => {
-    console.log(`Phrase ${index} - phrase: ${phrase}`);
+    console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 });
